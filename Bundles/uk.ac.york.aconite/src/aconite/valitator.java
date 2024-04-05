c4dba09a80f593166389c60a0149cc0fea7a67bd
N.W.Richardson <nr823@york.ac.uk>
2024-04-05 18:15:25 +0100

    Beginning of EVL

---
+++ Bundles/uk.ac.york.aconite/src/aconite/valitator.java
@@ -0,0 +1,7 @@
+package aconite;
+
+import org.eclipse.epsilon.evl.emf.validation.EvlValidator;
+
+public class valitator extends EvlValidator {
+
+}
--- runtime-Bundles/eugenia.experiment.design/src/module-info.java
+++ runtime-Bundles/eugenia.experiment.design/src/module-info.java
@@ -1,3 +1,4 @@
 module uk.ac.york.aconite.family {
 	exports eugenia.experiment.design;
+	requires org.eclipse.sirius;
 }
